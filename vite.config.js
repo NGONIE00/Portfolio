730e41072ea35d2a4c9f1988a30acb3d7048b9ee
NGONIE00 <119706548+NGONIE00@users.noreply.github.com>
2026-01-28 15:29:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: mode === 'development', // dev only
+      refresh: mode === 'development',
     }),
   ],
   build: {
     manifest: true,
-    outDir: path.resolve(__dirname, 'public/build'),
-    emptyOutDir: true,      // clears old builds
-    assetsDir: '',          // ensures manifest.json is at root of build folder
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  server: {
-    hmr: {
-      host: 'localhost',
-    },
+    outDir: 'public/build',
+    emptyOutDir: true,
   },
 }));
